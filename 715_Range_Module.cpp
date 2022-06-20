2bd4a69c940b888e35d48f5849578f06b71f601d
chillaxplaya <lqwooh@gmail.com>
2022-06-21 06:28:23 +0800

    commit

--- 715_Range_Module.cpp
+++ 715_Range_Module.cpp
@@ -1,2 +1,28 @@
 // @algorithm @lc id=715 lang=cpp 
 // @title range-module
+class RangeModule {
+public:
+    RangeModule() {
+        
+    }
+    
+    void addRange(int left, int right) {
+
+    }
+    
+    bool queryRange(int left, int right) {
+
+    }
+    
+    void removeRange(int left, int right) {
+
+    }
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
